6c95d041e7aa9f89714d1e250dde2ea1e4a43dd2
Anoop <socceranoo@gmail.com>
2013-11-20 21:42:32 -0800

    Added Jqplot.
      jqplot for activities
      tables.
      added heat map for hours

--- assets/js/mygitstats.js
+++ assets/js/mygitstats.js
@@ -37,33 +37,14 @@ function Tags($scope) {
 function display(data) {
 	var keys = Object.keys(data);
 	for (i = 0; i<keys.length; i++) {
-		//alert(keys[i]+" "+JSON.stringify(data[keys[i]]));
-		if (data[keys[i]].type && data[keys[i]].type == 'table')
+		if (data[keys[i]].type && data[keys[i]].type == 'table') {
 			newdatatable(data[keys[i]]);
+		} else if (data[keys[i]].type && data[keys[i]].type == 'bar') {
+			//alert(keys[i]+" "+JSON.stringify(data[keys[i]]));
+			init_bar_graph(data[keys[i]]);
+		} else if (data[keys[i]].type && data[keys[i]].type == 'pie') {
+			init_pie_chart(data[keys[i]]);
+		}
 	}
 }
 
-function newdatatable(table_obj) {
-	/* Data set - can contain whatever information you want */
-	var rows = [
-		['Misc','NetFront 3.1','Embedded devices','-','C'],
-		['Other browsers','All others','-','-','U']
-	];
-	var column_headers = [
-			{ "sTitle": "Engine" },
-			{ "sTitle": "Browser" },
-			{ "sTitle": "Platform" },
-			{ "sTitle": "Version", "sClass": "center" },
-			{ "sTitle": "Grade", "sClass": "center" }
-		];
-	if (table_obj.rows.length === 0)
-		return;
-	$('#'+table_obj.divid).dataTable( {
-		"aaData": table_obj.rows,
-		"aoColumns":table_obj.column_headers,
-		"aaSorting":[[table_obj.sort_index, table_obj.sort_order]],
-		"bDestroy":true,
-		"iDisplayLength":table_obj.show_count
-	});	
-	$('#'+table_obj.divid).css('width', '');
-}
--- assets/js/table-graph-helper.js
+++ assets/js/table-graph-helper.js
@@ -6,6 +6,9 @@ function jqPlotChart(div, renderer, data) {
 	this.drawChart  = function () {
 		var plot1 =  $.jqplot(this.chartDiv, this.chartData, this.chartOptions);
 	};
+	this.rePlot  = function () {
+		$.jqplot(this.chartDiv, this.chartData, this.chartOptions).replot();
+	};
 	this.setChartOptions  = function (key, value) {
 		this.chartOptions[key] = value;
 	};
@@ -43,91 +46,52 @@ function jqPlotChart(div, renderer, data) {
 /*
  Pass a javascript object to this function with the following members
  object = {
-	div_id: //Where the graph needs to appear
+	divid: //Where the graph needs to appear
 	horizontal:// true for horizontal graphs false for vertical graphs
 	title://What is the title of the graph
-	data://Json format data which is populated in data-json field in the DOM for that div 
-	entity://json data field where entity is specified
-	count://json data field where count is specified
+	xaxis: values
+	yaxis: values
 	upperlimit://upperlimit to limit the data
 	numberformat://format how axis values should appear like %d for decimal %.2f for two point floats etc
 */	
 function init_bar_graph(div_object) {
 	//alert(JSON.stringify(div_object));
-	var obj = div_object.json_data;
 	var upperlimit = div_object.upperlimit;
-	var limit = 0;
-	if (!obj) {
-		limit = 0;
-	} else {
-		limit = (obj.length > upperlimit)?upperlimit:obj.length;
-	}
-	var x_entity, y_entity;
-	var chart_values = {"xaxis": [], "yaxis":[]};
-	for (var i = 0; i < limit; i++) {
-		x_entity = obj[i][div_object.entity];
-		/*
-		 * if (module_hash[x_entity]) {
-		 *x_entity = module_hash[x_entity];
-		 * }
-		 */
-		y_entity = obj[i][div_object.count];
-		if (div_object.numberformat == "%.2f") {
-			y_entity = parseFloat(y_entity).toFixed(2);
-		}
-		if (div_object.horizontal) {
-			chart_values.xaxis.unshift(x_entity);
-			chart_values.yaxis.unshift(y_entity);
-			//chart_values.xaxis.push(x_entity);
-			//chart_values.yaxis.push(y_entity);
-		} else {
-			chart_values.xaxis.push(x_entity);
-			chart_values.yaxis.push(y_entity);
+	var limit = (div_object.yaxis.value.length > upperlimit)?upperlimit:div_object.yaxis.value.length;
+	var chart_values = {"xaxis": div_object.xaxis.value.slice(0, limit), "yaxis":div_object.yaxis.value.slice(0, limit)};
+	if (div_object.horizontal) {
+		for (var i = 0; i < limit; i++) {
+			chart_values.xaxis.unshift(div_object.xaxis.value[i]);
+			chart_values.yaxis.unshift(div_object.yaxis.value[i]);
 		}
 	}
-	var color_arr = get_random_colors(master_color_array, limit);
-	var chart_options = {div_id:div_object.div_id, horizontal:div_object.horizontal, xaxis_values:chart_values.xaxis, yaxis_values:chart_values.yaxis, graph_title:div_object.title, color_arr:color_arr, axesformat:div_object.numberformat, ylabel:div_object.ylabel};
-	draw_bar_graph(chart_options);
-}
-function draw_bar_graph(values) {
-	//alert(JSON.stringify(values));
-	$("#"+values.div_id).html("");
-	if (values.yaxis_values.length === 0) {
-		var newdiv ="<h3 class=text-center>"+values.graph_title+"</h3><div class=topdivider2></div><h2 class=text-center>Data not available</h2>";
-		$("#"+values.div_id).html(newdiv);
-		return;
-	}
-	$.jqplot.config.enablePlugins = true;
-	var general = new jqPlotChart(values.div_id, $.jqplot.BarRenderer, [values.yaxis_values]);
-	var legend = { show:true, placement: 'outside', rendererOptions: { numberRows: 1, animate:{show:true}}, location:'s', marginTop: '7px' };
 	var numpoints = 5;
-	var max = Math.max.apply(Math, values.yaxis_values);
-	var min = Math.min.apply(Math, values.yaxis_values);
-	var range = max - min;
-	var step = max/numpoints;
-	/*
-	if (values.axesformat == "%d") {
-		step = parseInt(step, 10);
-	} else if (values.axesformat == "%.2f") {
-		step = parseFloat(step).toFixed(2);
-	}
-   */
+	var max = Math.max.apply(Math, chart_values.yaxis);
+	var min = Math.min.apply(Math, chart_values.yaxis);
 	var min_y = 0;
+	var step = max/numpoints;
 	var max_y = step * (numpoints + 1);
+	var color_arr = get_random_colors(master_color_array, limit);
+	var values = {divid:div_object.divid, horizontal:div_object.horizontal, xaxis_values:chart_values.xaxis, yaxis_values:chart_values.yaxis, graph_title:div_object.title, color_arr:color_arr, numberformat:div_object.numberformat, xlabel:div_object.xaxis.label, ylabel:div_object.yaxis.label, axis_min:min_y, axis_max:max_y};
+	//alert(JSON.stringify(values));
+	$("#"+values.divid).html("");
+	$.jqplot.config.enablePlugins = true;
+	var general = new jqPlotChart(values.divid, $.jqplot.BarRenderer, [values.yaxis_values]);
+	var legend = { show:true, placement: 'outside', rendererOptions: { numberRows: 1, animate:{show:true}}, location:'s', marginTop: '7px' };
 	general.setChartOptions("title", values.graph_title);
 	if (values.horizontal) {
 		general.setChartOptions("axes", {yaxis: {renderer: $.jqplot.CategoryAxisRenderer, ticks: values.xaxis_values}});
-		general.setChartLevel2Options("axes", "xaxis", {min:min_y, max:max_y, label:values.ylabel, labelRenderer: $.jqplot.CanvasAxisLabelRenderer, labelOptions:{fontSize:'14pt'}});
+		general.setChartLevel2Options("axes", "xaxis", {min:values.axis_min, max:values.axis_max, label:values.ylabel, labelRenderer: $.jqplot.CanvasAxisLabelRenderer, labelOptions:{fontSize:'14pt'}});
 		general.setChartLevel3Options("seriesDefaults", "rendererOptions", "barDirection", "horizontal");
 		general.setChartLevel3Options("seriesDefaults", "rendererOptions", "barMargin", 20);
 	} else {
-		general.setChartOptions("axes", {xaxis: {renderer: $.jqplot.CategoryAxisRenderer, ticks: values.xaxis_values, tickRenderer: $.jqplot.CanvasAxisTickRenderer, tickOptions:{textColor:"#0088cc", angle:-20, fontSize:'14pt'}}});
-		general.setChartLevel2Options("axes", "yaxis", {min:min_y, max:max_y, label:values.ylabel, labelRenderer: $.jqplot.CanvasAxisLabelRenderer, labelOptions:{fontSize:'14pt'}});
+		general.setChartOptions("axes", {xaxis: {renderer: $.jqplot.CategoryAxisRenderer, ticks: values.xaxis_values, tickRenderer: $.jqplot.CanvasAxisTickRenderer, tickOptions:{textColor:"#0088cc", angle:-20, fontSize:'14pt'}, label:values.xlabel}});
+		general.setChartLevel2Options("axes", "yaxis", {min:values.axis_min, max:values.axis_max, label:values.ylabel, labelRenderer: $.jqplot.CanvasAxisLabelRenderer, labelOptions:{fontSize:'14pt'}});
 		general.setChartLevel3Options("seriesDefaults", "rendererOptions", "barMargin", 30);
 	}
 	general.setChartOptions("highlighter", {show:false, sizeAdjust:7, showtooltip:true, tooltipAxes:'both'});
 	general.setChartOptions("cursor", {show:false});
-	general.setChartOptions("axesDefaults", { min: min_y, tickOptions: { formatString: values.axesformat}});
+	general.setChartOptions("axesDefaults", { min: values.axis_min, tickOptions: { formatString: values.numberformat}});
 	general.setChartOptions("animate", !$.jqplot.use_excanvas);
 	general.setChartOptions("seriesColors", values.color_arr);
 	general.setChartLevel3Options("seriesDefaults", "rendererOptions", "varyBarColor", true);
@@ -137,6 +101,35 @@ function draw_bar_graph(values) {
 	general.drawChart();
 }
 
+function init_pie_chart(div_object) {
+	$.jqplot.config.enablePlugins = true;
+	var limit = (div_object.data.value.length > div_object.upperlimit)?div_object.upperlimit:div_object.data.value.length;
+	var spie = [['C/C++', 30], ['PHP', 30], ['PYTHON', 10], ['PERL', 20], ['MYSQL', 5], ['SHELL', 5]];
+	var data = div_object.data.value.slice(0, limit);
+	var backEnd = new jqPlotChart(div_object.divid, $.jqplot.PieRenderer, [data]);
+	var legend = { show:true, rendererOptions: {location:'e'}};
+	//var legend = { show:true, rendererOptions: { animate:{show:true}}, location:'e', marginTop: '7px' };
+	//var legend = { show:false};
+	var color_arr = get_random_colors(master_color_array, limit);
+	backEnd.setChartOptions("title", div_object.title);
+	backEnd.setChartOptions("legend", legend);
+	backEnd.setChartOptions("highlighter", { show: false });
+	backEnd.setChartOptions("animate", !$.jqplot.use_excanvas);
+	backEnd.setChartOptions("seriesColors", color_arr);
+	backEnd.setChartOptions("cursor", {show:false});
+	backEnd.setChartLevel3Options("seriesDefaults", "rendererOptions", "barMargin", 20);
+	backEnd.setChartLevel2Options("grid", "borderWidth", 0);
+	backEnd.setChartLevel2Options("grid", "shadow", false);
+	backEnd.drawChart();
+
+	//$("#"+div_object.divid).html($("#"+div_object.divid).html()+"<a id=change-colors class='btn btn-primary'>Change Colors</a>");
+
+	$("#change-colors").click(function () {
+		backEnd.setChartOptions("seriesColors", get_random_colors(master_color_array, limit));
+		backEnd.rePlot();
+	});
+}
+/*
 function draw_timeline_graph (hits, hotspots, title) {
 	$("#chart2").html("");
 	//var line2 = [['2004', 42], ['2005', 56], ['2008', 39], ['2010', 81]];
@@ -209,4 +202,30 @@ function draw_timeline_graph (hits, hotspots, title) {
 		}
 	});
 
+}
+*/
+
+function newdatatable(table_obj) {
+	/* Data set - can contain whatever information you want */
+	var rows = [
+		['Misc','NetFront 3.1','Embedded devices','-','C'],
+		['Other browsers','All others','-','-','U']
+	];
+	var column_headers = [
+			{ "sTitle": "Engine" },
+			{ "sTitle": "Browser" },
+			{ "sTitle": "Platform" },
+			{ "sTitle": "Version", "sClass": "center" },
+			{ "sTitle": "Grade", "sClass": "center" }
+		];
+	if (table_obj.rows.length === 0)
+		return;
+	$('#'+table_obj.divid).dataTable( {
+		"aaData": table_obj.rows,
+		"aoColumns":table_obj.column_headers,
+		"aaSorting":[[table_obj.sort_index, table_obj.sort_order]],
+		"bDestroy":true,
+		"iDisplayLength":table_obj.show_count
+	});	
+	$('#'+table_obj.divid).css('width', '');
 }
